79944ec823ae70367b1767e656e1486cb7c6aad8
qiaojialin <646274302@qq.com>
2022-06-12 15:40:30 +0800

    change schema region to ratis

--- integration-test/src/main/java/org/apache/iotdb/it/env/ConfigNode.java
+++ integration-test/src/main/java/org/apache/iotdb/it/env/ConfigNode.java
@@ -99,7 +99,7 @@ public class ConfigNode extends ClusterNodeBase {
       configProperties.setProperty("consensus_port", String.valueOf(this.consensusPort));
       configProperties.setProperty("target_confignode", this.targetConfignode);
       configProperties.setProperty(
-          "config_node_consensus_protocol_class",
+          "schema_region_consensus_protocol_class",
           "org.apache.iotdb.consensus.ratis.RatisConsensus");
       configProperties.setProperty(
           "data_region_consensus_protocol_class",
